11b51a53c232245433a8ad99a9b5d73326cd51e1
Yilena <3194927054@qq.com>
2025-05-11 20:15:16 +0800

    2025/5/11——加入敏感词过滤功能

--- Y-service/src/main/java/com/yilena/service/entity/dto/HistoryDTO.java
+++ Y-service/src/main/java/com/yilena/service/entity/dto/HistoryDTO.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -12,7 +13,7 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class HistoryDTO {
+public class HistoryDTO implements Serializable {
     private String dateStr;
     private List<Long> entityIds;
     private List<VideoVO> videoVOS;
